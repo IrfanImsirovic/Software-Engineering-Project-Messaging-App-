a0d9ab116df1fb44da29d33c3146d3c472c10530
Faruk Joldic <faruk.joldic@unravel.team>
2025-06-02 22:45:27 +0200

    Remove: Comments and Logs

--- demo/src/main/java/com/example/demo/config/WebConfig.java
+++ demo/src/main/java/com/example/demo/config/WebConfig.java
@@ -19,8 +19,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         Path uploadPath = Paths.get(uploadDir).toAbsolutePath().normalize();
         
-        System.out.println("📂 Adding resource handler for uploads at: " + uploadPath);
-        
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath + "/");
     }
